b99552a3998240b34b3bd6a696915b915d30b91c
Shahriar Ahmed <ourshahriar@gmail.com>
2023-06-11 13:22:32 +0600

    Login

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,30 @@
+import ErrorEl from "../components/Error/ErrorEl";
 import Root from "../layout/Root";
+import Home from "../pages/Home/Home";
+import Login from "../pages/LoginReg/Login";
+import Register from "../pages/LoginReg/Register";
 
 const { createBrowserRouter } = require("react-router-dom");
 
 const router= createBrowserRouter([
     {
         path:'/',
-        element:<Root/>
+        element:<Root/>,
+        errorElement:<ErrorEl/>,
+        children:[
+            {
+                path:'/',
+                element:<Home/>
+            },
+            {
+                path:'/login',
+                element:<Login/>
+            },
+            {
+                path:'/register',
+                element:<Register/>
+            }
+        ]
     }
 ])
 
